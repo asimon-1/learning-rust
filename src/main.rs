effb9cab81a6728f9904aa354301d3b9f66553c8
Andrew Simon <asimon@domeng.com>
2020-11-02 11:24:02 -0500

    Rectangle size comparison

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,15 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.area() > other.area() && self.height > other.height && self.width > other.width
+    }
 }
 
 #[derive(Debug)]
 struct Circle {
-    radius: u32
+    radius: u32,
 }
 
 impl Circle {
@@ -26,17 +30,17 @@ fn main() {
         width: 30,
         height: 50,
     };
-
-    let circ1 = Circle {
-        radius: 5,
+    let rect2 = Rectangle {
+        width: 2,
+        height: 51,
     };
+
+    let circ1 = Circle { radius: 5 };
 
     println!(
         "The area of the rectangel is {} square pixels.",
         rect1.area()
     );
-    println!(
-        "The area of the circle is {} square pixels.",
-        circ1.area()
-    );
+    println!("The area of the circle is {} square pixels.", circ1.area());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
 }
