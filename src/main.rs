7793accc5e16a95a9dbe6bf28453003ac68966f7
Andrew Simon <asimon@domeng.com>
2020-11-02 11:01:04 -0500

    Add circle to struct example

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,33 @@ impl Rectangle {
     }
 }
 
+#[derive(Debug)]
+struct Circle {
+    radius: u32
+}
+
+impl Circle {
+    fn area(&self) -> f32 {
+        3.14159 * (self.radius as f32).powf(2.0)
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
+    let circ1 = Circle {
+        radius: 5,
+    };
+
     println!(
         "The area of the rectangel is {} square pixels.",
         rect1.area()
+    );
+    println!(
+        "The area of the circle is {} square pixels.",
+        circ1.area()
     );
 }
