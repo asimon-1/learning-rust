f824065a5553058b36ecae657fd5734e75815376
Andrew Simon <asimon@domeng.com>
2020-11-06 10:21:53 -0500

    Refactor on tasks app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io;
 use std::fmt;
+use std::io;
 
 #[derive(Debug)]
 struct Task {
@@ -10,12 +10,12 @@ struct Task {
 
 impl fmt::Display for Task {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let check = if self.completed {"X"} else {" "};
+        let check = if self.completed { "X" } else { " " };
         write!(f, "[{}] Task {}: {}", check, self.id, self.text)
     }
 }
 
-fn new_task(id: u32, text: String) -> Task {
+fn create_task(id: u32, text: String) -> Task {
     Task {
         id,
         completed: false,
@@ -23,19 +23,37 @@ fn new_task(id: u32, text: String) -> Task {
     }
 }
 
+fn list_all_tasks(task_list: std::vec::Vec<Task>) {
+    println!("Current Task List:");
+    for t in task_list {
+        println!("> {}", t);
+    }
+}
+
 fn main() {
     let mut task_list: Vec<Task> = Vec::new();
-    for id in 0..5 {
-        println!("Please enter the task to be completed.");
-        let mut text = String::new();
-        io::stdin()
-            .read_line(&mut text)
-            .expect("Failed to read line.");
-        task_list.push(new_task(id, text));
-    }
+    let mut id: u32 = 0;
 
-    println!("Contents of task_list:");
-    for t in task_list.iter() {
-        println!("> {}", t);
+    loop {
+        println!("What would you like to do?\n[n] New Task\n[l] List Tasks\n[c] Mark Task as Complete\n[q] Quit");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line.");
+        match input.trim_end() {
+            "n" => {
+                println!("Please enter the task to be completed.");
+                let mut text = String::new();
+                io::stdin()
+                    .read_line(&mut text)
+                    .expect("Failed to read line.");
+                id += 1;
+                task_list.push(create_task(id, text));
+            }
+            "l" => list_all_tasks(task_list),
+            // "c" => complete_task(task_list), // Not yet written
+            "q" => break,
+            _ => continue,
+        }
     }
 }
