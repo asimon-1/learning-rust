5895b2ada1a1a83db370389ae603eb6ae94ec5f7
Andrew Simon <asimon@domeng.com>
2020-11-12 10:37:15 -0500

    Extract regex into a function and fix invalid elements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-// use std::env;
 use std::fs;
 use std::path::Path;
 use regex::Regex;
+
+fn apply_transform(contents: &String) -> String {
+    let re_base = Regex::new(r#"<attribute name="(?P<element>.+?)" value="(?P<value>.*?)"/>"#).unwrap();
+    let re_clean = Regex::new(r#"(</?\w+)\s(\w+>)"#).unwrap();
+
+    let base = re_base.replace_all(contents,"<$element>$value</$element>");
+    return re_clean.replace_all(&base, "$1$2").to_string()
+
+}
 
 fn main() {
     let filename = "test.XML";
@@ -14,8 +22,7 @@ fn main() {
 
     println!("With text:\n{}", contents);
 
-    let re = Regex::new(r#"<attribute name="(?P<element>.+?)" value="(?P<value>.*?)".*>"#).unwrap();
-    let result = re.replace_all(&contents, "<$element>$value</$element>");
+    let result = apply_transform(&contents);
     println!("{}", result);
     fs::write(filename, &*result).expect("Unable to write to file")
 }
