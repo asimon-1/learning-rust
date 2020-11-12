6a65630b4380f09bda4609810c7d2ad46bc27f12
Andrew Simon <asimon@domeng.com>
2020-11-12 11:32:10 -0500

    Add some error handling

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,44 @@
-use std::fs;
-use std::path::Path;
 use regex::Regex;
+use std::ffi::OsStr;
+use std::fs;
 
 fn apply_transform(contents: &String) -> String {
-    let re_base = Regex::new(r#"<attribute name="(?P<element>.+?)" value="(?P<value>.*?)"/>"#).unwrap();
+    let re_base =
+        Regex::new(r#"<attribute name="(?P<element>.+?)" value="(?P<value>.*?)"/>"#).unwrap();
     let re_clean = Regex::new(r#"(</?\w+)\s(\w+>)"#).unwrap();
 
-    let base = re_base.replace_all(contents,"<$element>$value</$element>");
-    return re_clean.replace_all(&base, "$1$2").to_string()
-
+    let base = re_base.replace_all(contents, "<$element>$value</$element>");
+    return re_clean.replace_all(&base, "$1$2").to_string();
 }
 
 fn main() {
-    let filename = "test.XML";
-    let path = Path::new(&filename);
-    let display = path.display();
-    println!("In file {}", display);
+    let paths = fs::read_dir("./").unwrap();
+    for path in paths {
+        let path = path.unwrap().path();
+        let display = path.display();
+        println!("Checking path {}", display);
+        if path.is_dir() {
+            println!("Skipping, {} is a directory.", display);
+            continue;
+        } else {
+            match path.extension().and_then(OsStr::to_str) {
+                Some(ext) => {
+                    if ext.to_lowercase() == "xml" {
+                        println!("Converting file: {}", display);
+                        let contents = fs::read_to_string(&path)
+                            .expect("Something went wrong reading the file");
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-
-    let result = apply_transform(&contents);
-    println!("{}", result);
-    fs::write(filename, &*result).expect("Unable to write to file")
+                        let result = apply_transform(&contents);
+                        fs::write(path, &*result).expect("Unable to write to file")
+                    } else {
+                        println!("Skipping, {} is not an XML file.", display);
+                    }
+                }
+                _ => {
+                    println!("Skipping, could not parse extension for {}.", display);
+                    continue;
+                }
+            };
+        }
+    }
 }
